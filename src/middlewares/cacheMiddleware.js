c3bf9c5d7fb8b731c5bf35460b5ec2fa7cd12ee5
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-01 10:38:36 +0530

    added redisClient and cacheMiddleware

---
+++ src/middlewares/cacheMiddleware.js
@@ -0,0 +1,21 @@
+const redisClient = require("../utils/redisClient");
+
+const cacheMiddleware = async (req, res, next) => {
+  const cacheKey = req.originalUrl; 
+
+  try {
+    const cachedData = await redisClient.get(cacheKey);
+
+    if (cachedData) {
+      console.log("Serving from cache");
+      return res.json(JSON.parse(cachedData)); 
+    }
+
+    next();
+  } catch (error) {
+    console.error("Redis Cache Error:", error);
+    next(); 
+  }
+};
+
+module.exports = cacheMiddleware;
