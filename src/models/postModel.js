131a9aa167b3758fa9d65e107515c018861a2ec5
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-01 09:13:42 +0530

    faq posting model created

---
+++ src/models/postModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    question: { type: String, required: true },
+    answer: { type: String, required: true },
+    translations: {
+      type: Map,
+      of: String,
+    },
+  },
+  { timestamps: true }
+);
+
+postSchema.methods.getTranslation = function (lang) {
+  return this.translations[`${lang}`] || this.question;
+};
+
+const FAQ = mongoose.model("FAQ", postSchema);
+module.exports = FAQ;
